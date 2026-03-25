057f1593395c8ddd5e50aa22f1c873f347e204c2
Mary <123047616@upq.edu.mx>
2026-03-24 18:04:18 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
 from app import app
-from contacts import contacts
+from app.contacts import contacts
 
 app.register_blueprint(contacts)
-
-# starting the app
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
